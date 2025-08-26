7d596fa04956d5e7225ec8212523ab4dc432ec3d
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-26 23:05:07 +0300

    Add files via upload

--- Week3PythonAssignment.py
+++ Week3PythonAssignment.py
@@ -8,4 +8,4 @@ def calculate_discount(price, discount_percent):
     """
     if discount_percent >= 20:
         discount_amount = (discount_percent / 100) * price
-        
+        return price - discount_amount        
