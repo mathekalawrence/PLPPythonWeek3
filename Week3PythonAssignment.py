9eba4947ec22aafab3c6d14a0d891d5a8924f090
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-26 23:03:39 +0300

    Add files via upload

--- Week3PythonAssignment.py
+++ Week3PythonAssignment.py
@@ -1,1 +1,11 @@
-#Python
+#Python Week3 Assignment
+
+def calculate_discount(price, discount_percent):
+    """
+    Calculating the final price after a discount.
+    If the discount is less than 20%, then it has to return the originaal price
+
+    """
+    if discount_percent >= 20:
+        discount_amount = (discount_percent / 100) * price
+        
