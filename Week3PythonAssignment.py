2067e4c03b45399850ecb666c94c1c8aff1b7e52
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-28 15:48:55 +0300

    Add files via upload

--- Week3PythonAssignment.py
+++ Week3PythonAssignment.py
@@ -6,11 +6,13 @@ def calculate_discount(price, discount_percent):
     If the discount is less than 20%, then it has to return the originaal price
 
     """
+    #Checking if the discount is 20% or higher
     if discount_percent >= 20.0:
         #Calculating the discount amount
         discount_amount = (discount_percent / 100) * price
         #Calculating the final price
-        return price - discount_amount  
+        final_price = price - discount_amount
+        return final_price
     else:
         return price
 
